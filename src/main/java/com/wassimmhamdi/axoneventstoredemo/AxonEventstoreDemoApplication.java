729bdb7c195944361eca6d1fcd1e3335982153d3
wam <wam@onesty-tech.de>
2020-04-08 13:43:45 +0200

    init commit

---
+++ src/main/java/com/wassimmhamdi/axoneventstoredemo/AxonEventstoreDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wassimmhamdi.axoneventstoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonEventstoreDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxonEventstoreDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wassimmhamdi/axoneventstoredemo/AxonEventstoreDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wassimmhamdi.axoneventstoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxonEventstoreDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
